76e5b97f1cb399e81f5be595a3cc7cf16f557111
Pietro Di Bello <pietro.dibello@xpeppers.com>
2014-05-14 22:35:53 +0200

    Handling negative numbers

--- src/com/xpeppers/www/kata/StringCalculator.java
+++ src/com/xpeppers/www/kata/StringCalculator.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class StringCalculator {
 
-    public static int add(String stringOfNumbers) {
+    public static int add(String stringOfNumbers) throws Exception {
         String[] numbers = extractNumbersFrom(stringOfNumbers);
 
         return sum(numbers);
@@ -26,13 +26,20 @@ public class StringCalculator {
         return stringOfNumbers.split(delimitersRegexp);
     }
 
-    private static int sum(String[] numbers) {
+    private static int sum(String[] numbers) throws Exception {
         int sum = 0;
         for (String eachNumber : numbers) {
-            sum += Integer.parseInt(eachNumber);
+            int eachInteger = Integer.parseInt(eachNumber);
+            raiseExceptionForNegativeNumber(eachInteger);
+            sum += eachInteger;
         }
 
         return sum;
+    }
+
+    private static void raiseExceptionForNegativeNumber(int integer) throws Exception {
+        if (integer < 0)
+            throw new Exception("negatives not allowed: " + integer);
     }
 
     private static Matcher match(String stringOfNumbers, String regex) {
--- test/com/xpeppers/www/kata/StringCalculatorTest.java
+++ test/com/xpeppers/www/kata/StringCalculatorTest.java
@@ -1,13 +1,15 @@
 package com.xpeppers.www.kata;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
 public class StringCalculatorTest {
 
     @Test
-    public void emptyStringAddsToZero() {
+    public void emptyStringAddsToZero() throws Exception {
         assertEquals(0, StringCalculator.add(""));
     }
 
@@ -35,4 +37,25 @@ public class StringCalculatorTest {
     public void stringWithCustomerDelimiterAndNoNumersAddsToZero() throws Exception {
         assertEquals(0, StringCalculator.add("//;\n"));
     }
+
+    @Test
+    public void throwsExceptionForNegativeNumbers() throws Exception {
+        try {
+            StringCalculator.add("1,2,-3");
+            fail("Should never reach this line!");
+        } catch (Exception e) {
+        }
+    }
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void throwsExceptionForNegativeNumbersWithProperMessage() throws Exception {
+        expectedException.expect(Exception.class);
+        expectedException.expectMessage("negatives not allowed: -3");
+
+        StringCalculator.add("1,2,-3");
+    }
+
 }
